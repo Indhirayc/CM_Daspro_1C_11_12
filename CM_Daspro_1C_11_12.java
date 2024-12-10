3685651384e796294aa71e63fe44a08f823b4aa3
MadeOka06 <okadarmestha@gmail.com>
2024-12-10 13:15:17 +0700

    Menampilkan Menu
    I Gusti Agung Made Oka Darmestha

---
+++ CM_Daspro_1C_11_12.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CM_Daspro_1C_11_12 {
+    static Scanner daspro1112 = new Scanner(System.in);
+    static String [] namaBarang = new String[10];
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("===== MENU INVENTORI CAFE =====");
+            System.out.println("1. Tampilkan Inventori");
+            System.out.println("2. Tambah Stok untuk item yang ada");
+            System.out.println("3. Tambah Item Baru");
+            System.out.println("4. Keluar");
+            System.out.print("Pilih Menu: ");
+            int menu = daspro1112.nextInt();
+        }
+    }
+
+}
